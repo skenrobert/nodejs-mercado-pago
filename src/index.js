7ae02cedb9e768c7d57d10e64fc8857728d2789b
kenny_robert <skenrobert@gmail.com>
2023-06-11 16:26:08 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import morgan from 'morgan';
 import paymentRoutes from './routes/payment.js'; //all modulo used .js is make for me
+import { PORT } from './routes/config.js';
 
 const app = express();
+app.use(morgan('dev')); // show in console, put,get, post, delete
 
 app.use(paymentRoutes);
 
